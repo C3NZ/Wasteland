f0153bda9fa714e43805125fe0231fac43f8ffa2
C3NZ <cenz@cenz.io>
2018-11-16 14:01:33 -0800

    [etc] move work to desktop

--- public/js/Game.js
+++ public/js/Game.js
@@ -24,8 +24,40 @@ class Game {
         this.ui = new UI(this);
         this.eventManager = new Event(this)
         this.caravan = new Caravan(this, stats);
+        this.world = new World() 
+        this.init();
     }
     init() {
+        this.startJourney();
+    }
+
+    startJourney() {
+        this.running = true;
+        this.previousTime = false;
+        this.ui.notify('The journey through the wasteland begins', 'positive');
+        this.step();
+    }
+
+    step(timestamp) {
+        if(!this.previousTime){
+            this.previousTime = timestamp;
+            this.update();
+        }
+
+        const progress = timestamp - this.previousTime;
+
+        if(progress >= Config.GAME_SPEED) {
+            this.previousTime = timestamp;
+            this.updateGame();
+        }
+
+        if (this.gameActive) {
+            window.requestAnimationFrame(this.step.bind(this))
+        }
+    }
+
+    updateGame() {
+        this.caravan.day += Config.DAY_PER_STEP;
 
     }
 }
