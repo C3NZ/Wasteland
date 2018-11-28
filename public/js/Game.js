8a328adb51e5ebf69af9ea99e87ffc8d02e80dc9
C3NZ <cenz@cenz.io>
2018-11-27 17:25:23 -0800

    [feat] add game loop and some basic game logic to get the game started

--- public/js/Game.js
+++ public/js/Game.js
@@ -1,6 +1,7 @@
 import {Caravan} from './Caravan.js';
 import {UI} from './UI.js';
 import {Event} from './Event.js';
+import {Config} from './Config.js';
 
 function startGame(stats) {
     if(stats) {
@@ -9,7 +10,7 @@ function startGame(stats) {
         const stats = {
             day: 0,
             distance: 0,
-            crew: 30,
+            travelers: 30,
             food: 80,
             oxen: 2,
             money: 300,
@@ -24,40 +25,92 @@ class Game {
         this.ui = new UI(this);
         this.eventManager = new Event(this)
         this.caravan = new Caravan(this, stats);
-        this.world = new World() 
+        //this.world = new World() 
         this.init();
     }
+
+    //start the journey
     init() {
         this.startJourney();
     }
 
+    //Start the journey and entire game
     startJourney() {
         this.running = true;
         this.previousTime = false;
         this.ui.notify('The journey through the wasteland begins', 'positive');
         this.step();
     }
-
+    
+    //Game loop
     step(timestamp) {
+
+        //first step, instantiate the previous time and call our update function
         if(!this.previousTime){
             this.previousTime = timestamp;
             this.update();
         }
-
+        
+        //Find the time elapsed
         const progress = timestamp - this.previousTime;
-
+        
+        //Make sure that the game isn't updating too fast 
         if(progress >= Config.GAME_SPEED) {
             this.previousTime = timestamp;
-            this.updateGame();
+            this.update();
         }
 
-        if (this.gameActive) {
+        //Request another call as soon as the window allows us
+        if (this.running) {
             window.requestAnimationFrame(this.step.bind(this))
         }
     }
 
-    updateGame() {
+    update() {
+        //Update the day and consume food
         this.caravan.day += Config.DAY_PER_STEP;
+        this.caravan.consumeFood();
 
+        //End the game if there is no more food
+        if (this.caravan.food === 0) {
+            this.ui.notify('Your caravan has starved to death', 'negative');
+            this.running = false;
+            return;
+        }
+
+        //update weight, distance, and visual stats
+        this.caravan.updateWeight();
+        this.caravan.updateDistance();
+        this.ui.refreshStats();
+
+        //End game if there are no more travelers
+        if (this.caravan.travelers <= 0) {
+            this.caravan.travelers = 0;
+            this.ui.notify('Everyone has died', 'negative');
+            this.running === false;
+            return;
+        }
+
+        //End game if there we have reached our end distance
+        if (this.caravan.distance >= Config.FINAL_DISTANCE) {
+            this.ui.notify('You have made it across the wasteland!', 'positive');
+            this.running = false;
+            return;
+        }
+
+        //Random event logic will go here
+    }
+    
+    //Pause the journey
+    pauseJourney() {
+        this.running = false;
+    }
+
+    //Resume the journey
+    resumeJourney() {
+        this.running = true;
+        this.step();
     }
 }
+
+startGame();
