0b0dbd58c68e2366491b36302adea875abeeaa06
C3NZ <cenz@cenz.io>
2018-12-09 19:41:14 -0800

    [feat] add UI components to work with event system

--- public/js/UI.js
+++ public/js/UI.js
@@ -29,6 +29,114 @@ export class UI {
         // Update the caravan positions
         document.getElementById('caravan').style.left = `${380 * caravan.distance / Config.FINAL_DISTANCE}px`;
     }
+
+    // Show the player that they're being attacked by some savage bandits
+    showAttack(firepower, gold) {
+        const attackDiv = document.getElementById('attack');
+        attackDiv.classList.remove('hidden');
+
+        this.firepower = firepower;
+        this.gold = gold;
+
+        document.getElementById('attack-description').innerHTML = `firepower: ${firepower}`
+
+        if (!this.attackInitiated) {
+            document.getElementById('fight').addEventListener('click', this.fight.bind(this));
+
+            document.getElementById('runaway').addEventListener('click', this.runaway.bind(this));
+
+            this.attackInitiated = true
+        }
+    }
+
+    fight() {
+        const firepower = this.firepower;
+        const gold = this.firepower;
+
+        const damage = Math.ceil(Math.max(0, firepower * 2 * Math.random() - this.game.caravan.firepower));
+
+        if (damage < this.game.caravan.crew) {
+            this.game.caravan.crew -= damage;
+            this.game.caravan.money += gold;
+            this.notify(`${damage} people were killed fighting`, 'notify');
+            this.notify(`Found $${gold}`, 'gold');
+        } else {
+            this.game.caravan.crew = 0;
+            this.notify('Everybody died in the fight', 'negative');
+        }
+
+        document.getElementById('attack').classList.add('hidden');
+        this.game.resumeJourney();
+    }
+
+    runaway() {
+        const firepower = this.firepower;
+
+        const damage = Math.ceil(Math.max(0, firepower * Math.random() / 2));
+
+        if (damage < this.game.caravan.crew) {
+            this.game.caravan.crew -= damage;
+            this.notify(`${damage} people were killed fighting`, 'negative');
+        } else {
+            this.game.caravan.crew = 0;
+            this.notify('Everybody died running away', 'negative');
+        }
+
+        document.getElementById('runaway').removeEventListener('click');
+        document.getElementById('attack').classList.add('hidden');
+
+        this.game.resumeJourney();
+    }
+
+    showShop(products) {
+        const shopDiv = document.getElementById('shop');
+        shopDiv.classList.remove('hidden');
+
+        if (!this.shopInitiated) {
+            shopDiv.addEventListener('click', function(e) {
+                const target = e.target || e.src;
+                
+                // Exit button
+                if (target.tagName === 'BUTTON') {
+                    shopDiv.classList.add('hidden');
+                    this.game.resumeJourney();
+                // Allow the user to buy the product
+                } else if (target.tagName === 'DIV' && target.className.match(/product/)) {
+                    this.buyProduct({
+                        item: target.getAttribute('data-item'),
+                        qty: target.getAttribute('data-qty'),
+                        price: target.getAttribute('data-price'),
+                    });
+                }
+            }.bind(this));
+            this.shopInitiated = true;
+        }
+
+        const prodsDiv = document.getElementById('prods');
+        prodsDiv.innerHTML = '';
+
+        let product;
+        for (let i = 0; i < products.length; i += 1) {
+            product = products[i];
+            prodsDiv.innerHTML += `<div class="product" data-qty="${product.qty}" data-item="${product.item}" data-price="${product.price}"> ${product.qty} ${product.item} - $ ${product.price}</div>`;
+        }
+    }
+    
+    // buy product
+    buyProduct(product) {
+        if (product.price > this.game.caravan.money) {
+            this.notify('Not enough money', 'negative');
+            return false;
+        }
+
+        this.game.caravan.money -= product.price;
+        this.game.caravan[product.item] += +product.qty;
+        this.notify(`Bought ${product.qty} x ${product.item}`, 'positive');
+
+        this.game.caravan.updateWeight();
+        this.refreshStats();
+        return true;
+    }
 }
 
 export default UI;
